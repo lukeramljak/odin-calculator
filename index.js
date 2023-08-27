f02f2282708fc858cc998f58997853ee399ce789
Luke Ramljak <lukeramljak@proton.me>
2023-08-27 19:29:06 +0800

    feat: create fundamental functions

---
+++ index.js
@@ -0,0 +1,36 @@
+let num1 = 0;
+let operator = '';
+let num2 = 0;
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case '+':
+      add(num1, num2);
+      break;
+    case '-':
+      subtract(num1, num2);
+      break;
+    case '*':
+      multiply(num1, num2);
+      break;
+    case '/':
+      divide(num1, num2);
+      break;
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
