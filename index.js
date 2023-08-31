f2a7497f1649408352ab5c16be1b952c6bd9b5ca
Luke Ramljak <lukeramljak@proton.me>
2023-08-31 19:30:08 +0800

    refactor: redo clear/delete buttons

--- index.js
+++ index.js
@@ -11,12 +11,29 @@ const decimalButton = document.getElementById('decimalBtn');
 const lastOperationScreen = document.getElementById('lastOperationScreen');
 const currentOperationScreen = document.getElementById('currentOperationScreen');
 
+clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', deleteNumber);
+
 numberButtons.forEach((button) => {
   button.addEventListener('click', () => appendNumber(button.textContent));
 });
 
 function appendNumber(number) {
   currentOperationScreen.textContent += number;
+}
+
+function clear() {
+  currentOperationScreen.textContent = '0';
+  lastOperationScreen.textContent = '';
+  firstOperand = '';
+  secondOperand = '';
+  currentOperation = '';
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1);
 }
 
 function operate(operator, a, b) {
